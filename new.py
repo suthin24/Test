5e363636c30de35b2f88af6b2fcef23dea791ee5
Suthin <suthin0624@gmail.com>
2024-03-05 15:04:21 +0530

    added hungry prompt

--- new.py
+++ new.py
@@ -1,4 +1,6 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print('Im hungry')
-Im hungry
+hungry = input("are you hungry ? ")
+if hungry== "yes":
+    print('eat fruits')
+else:
+    print('do your homework')
+    
